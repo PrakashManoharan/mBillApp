938899d81fd6e182c18ea8a11ea301c252403271
JKBC1800 <31428649+JKBC1800@users.noreply.github.com>
2019-02-19 18:09:53 +1100

    Updates

---
+++ precache-manifest.4a61ebdceb18e3dd2512fa028c8a7f9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d71f21d6f425c8a1b0d6",
+    "url": "/mbillapp/static/js/runtime~main.d71f21d6.js"
+  },
+  {
+    "revision": "c45435afeeb812e74901",
+    "url": "/mbillapp/static/js/main.c45435af.chunk.js"
+  },
+  {
+    "revision": "f1320c0db08a9d99840a",
+    "url": "/mbillapp/static/js/2.f1320c0d.chunk.js"
+  },
+  {
+    "revision": "c45435afeeb812e74901",
+    "url": "/mbillapp/static/css/main.9948a7da.chunk.css"
+  },
+  {
+    "revision": "b48e46f43f9ed53f6dd6224571ed2564",
+    "url": "/mbillapp/index.html"
+  }
+];
