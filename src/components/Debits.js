dc6281fca703dbe8c3d38dc625459c552e3dc71f
JKBC1800 <31428649+JKBC1800@users.noreply.github.com>
2019-02-19 18:12:44 +1100

    Enter

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -9,7 +9,7 @@ const Debits = () => {
       <h1 className="deb-header">
         <span className="symbol">-</span>Debits
       </h1>
-      <h2 className="deb-value">{totalDebits}</h2>
+      <h5 className="deb-value">{totalDebits}</h5>
     </div>
   );
 };
